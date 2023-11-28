9975228e302bd2991fca22de9d68210e1a51f700
zhalil <zhalil.p.1303@gmail.com>
2023-11-28 11:02:12 +0300

    add validator for Product

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
@@ -7,24 +9,27 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
 class Product(models.Model):
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def save(self, *args, **kwargs):
-        if self.quantity < 0:
-            self.quantity = 0
-        elif self.quantity > 100000:
-            self.quantity = 100000
-        if self.price < 0:
-            self.price = 0
-        super().save(*args, **kwargs)
+    quantity = models.PositiveIntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100000)
+        ]
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
 
     def __str__(self):
         return self.title
-
 
     def get_category_hierarchy(self):
         hierarchy = []
